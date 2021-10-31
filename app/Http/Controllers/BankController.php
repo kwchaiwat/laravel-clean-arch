82202976fdaab20568ccafdfc6ca68819a36f472
Chaiwat Kaewmukdasawan <67299853+kwchaiwat@users.noreply.github.com>
2021-11-01 01:19:22 +0700

    refactory

--- app/Http/Controllers/BankController.php
+++ app/Http/Controllers/BankController.php
@@ -6,7 +6,6 @@ use App\Http\Requests\BankRequest;
 use App\Http\Resources\BankCollection;
 use App\Http\Resources\BankResource;
 use App\Repositories\Bank\BankRepository;
-use Illuminate\Support\Facades\Session;
 
 class BankController extends Controller
 {
@@ -17,19 +16,20 @@ class BankController extends Controller
         $this->bank = $bank;
     }
 
-    public function getAllBanks(){
+    public function getAllBanks(): BankCollection
+    {
         $banks = $this->bank->getAll();
         return new BankCollection($banks);
     }
 
-    public function getBank($id){
+    public function getBank($id): BankResource
+    {
         $bank =  $this->bank->getById($id);
         return new BankResource($bank);
     }
 
-    public function create(BankRequest $request){
-        $validator = $request->validated();
-
+    public function create(BankRequest $request): BankResource
+    {
         $bank =  $this->bank->create($request->safe()->only('account_number', 'trust'));
         return new BankResource($bank);
     }
